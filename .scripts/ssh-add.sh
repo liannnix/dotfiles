0172aae7b382a16e9716f4ad2579bc377d1ab17b
Andrey Limachko <liannnix@giran.cyou>
2024-07-26 02:24:17 +0400

    .scripts/ssh-add.sh: Add sh-add keys script

---
+++ .scripts/ssh-add.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+ssh-add ~/.ssh/id_rsa
+ssh-add ~/.ssh/id_ed25519
+ssh-add ~/.ssh/girar.key
+ssh-add ~/.ssh/gitea_jenkins
+ssh-add ~/.ssh/pve
+ssh-add ~/.ssh/pve_admin
+ssh-add ~/.ssh/robot_key
+
