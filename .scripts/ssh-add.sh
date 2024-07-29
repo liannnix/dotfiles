5baa95913eba3de02446702dc6a55bd5615ef2aa
Andrey Limachko <liannnix@giran.cyou>
2024-07-29 18:18:46 +0400

    .scripts/ssh-add.sh: Comment unused ssh keys

--- .scripts/ssh-add.sh
+++ .scripts/ssh-add.sh
@@ -3,8 +3,8 @@
 ssh-add ~/.ssh/id_rsa
 ssh-add ~/.ssh/id_ed25519
 ssh-add ~/.ssh/girar.key
-ssh-add ~/.ssh/gitea_jenkins
-ssh-add ~/.ssh/pve
+#ssh-add ~/.ssh/gitea_jenkins
+#ssh-add ~/.ssh/pve
 ssh-add ~/.ssh/pve_admin
-ssh-add ~/.ssh/robot_key
+#ssh-add ~/.ssh/robot_key
 
